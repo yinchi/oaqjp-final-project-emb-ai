87424867a0dea423970719d6304eceb37d041402
Yin-Chi Chan <yinchichan@pm.me>
2026-04-30 16:02:20 -0400

    Tasks 1 and 2

---
+++ emotion_detection.py
@@ -0,0 +1,9 @@
+import requests
+
+def emotion_detector(text_to_analyze: str):
+    response = requests.post(
+        'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict',
+        headers={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"},
+        json={ "raw_document": { "text": text_to_analyze } }
+    )
+    return response.text
